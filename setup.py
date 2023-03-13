911932c091aeee6897a40e32e3b261b643003fc2
jasperdupuis <jasperdupuis@gmail.com>
2023-03-13 11:55:36 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Code for MASC in ML and acoustics on Orca-class acoustic dataset.',
+    author='jasperdupuis',
+    license='MIT',
+)
